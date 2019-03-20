52a18000cf34c540547afd64fe79c709d9636d13
Vijay Gupta <48433645+VIjayGupta6572@users.noreply.github.com>
2019-03-20 14:48:51 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,24 @@
+function add()
+{
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    document.getElementById('res').innerHTML=+num1 + +num2;
+}
+function subtract()
+{
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    document.getElementById('res').innerHTML= (num1-num2);
+}
+function multiply()
+{
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    document.getElementById('res').innerHTML=(num1*num2);
+}
+function divide()
+{
+    var num1=document.getElementById("num1").value;
+    var num2=document.getElementById("num2").value;
+    document.getElementById("res").innerHTML=num1/num2;
+}
